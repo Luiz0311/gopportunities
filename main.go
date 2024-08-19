7906f7abc8ffbcac7b3bb22ade21b4b06aefa99f
Luiz0311 <luizhenriquediogo7@gmail.com>
2024-08-19 07:45:19 -0300

    feat: add handlers, schemas and configs

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"github.com/Luiz0311/gopportunities/config"
 	"github.com/Luiz0311/gopportunities/router"
 )
 
+var logger *config.Logger
+
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
--- router/routes.go
+++ router/routes.go
@@ -1,38 +1,17 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/Luiz0311/gopportunities/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(r *gin.Engine) {
 	api := r.Group("/api")
 	{
-		api.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Opening",
-			})
-		})
-		api.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "POST Opening",
-			})
-		})
-		api.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "DELETE Opening",
-			})
-		})
-		api.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "PUT Opening",
-			})
-		})
-		api.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "Openings",
-			})
-		})
+		api.GET("/opening", handler.ShowOpeningHandler)
+		api.POST("/opening", handler.CreateOpeningHandler)
+		api.DELETE("/opening", handler.DeleteOpeningHandler)
+		api.PUT("/opening", handler.UpdateOpeningHandler)
+		api.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
